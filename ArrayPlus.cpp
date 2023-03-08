abd9898146bd903e5d219d1390447b8e364ecb3e
SERGE GUEA <113424842+SERGE3-g@users.noreply.github.com>
2023-03-08 05:44:40 +0100

    Update ArrayPlus.cpp

--- ArrayPlus.cpp
+++ ArrayPlus.cpp
@@ -8,28 +8,17 @@
 using namespace std;
 
 // create
-int *aggiungiInFondo(int base[], int lunghezza, int nuovo)
+int *aggiungiInFondo(int base[], int dim, int nuovo)
 {
-    int lunghezzaNuovo = lunghezza + 1;
-    int arrayNuovo[lunghezzaNuovo];
-    for (int i = 0; i < lunghezzaNuovo + 1; i++)
+    int arrayRis[dim + 1];
+    for (int i = 0; i < dim; i++)
     {
-        arrayNuovo[i] = base[i];
+        arrayRis[i] = base[i];
     }
-    for (int i = 0; i < lunghezzaNuovo; i++)
-    {
-        if (i < lunghezzaNuovo)
-        {
-            arrayNuovo[lunghezzaNuovo - 1] = nuovo;
-        }
-    }
-    cout<< "Aggiungi in fondo il numero "<< nuovo <<endl;
-    for (int i = 0; i < lunghezzaNuovo; i++)
-    {
-        cout << arrayNuovo[i] << " ";
-    }
-
-    return arrayNuovo;
+    arrayRis[dim] = nuovo;
+    cout << "Aggiungo in fondo " << nuovo << endl;
+    mostra(arrayRis, dim + 1);
+    return arrayRis;
 }
 
 int *aggiungiInTesta(int base[], int lunghezza, int nuovo)
@@ -81,28 +70,43 @@ int *aggiungiInPosizione(int base[], int lunghezza, int nuovo, int posizione)
     return arrayNuovo;
 }
 
-int trovaPosizione(int base[], int lunghezza, int cerca)
+int trovaPosizione(int base[], int dim, int cerca) 
 {
-    int trovaPosizione = 3;
-    int arrayNuovo[trovaPosizione];
-    for (int i = 0; i < trovaPosizione; i++)
-        arrayNuovo[i] = base[i];
-    for (int i = 0; i < lunghezza; i++)
+    for (int i = 0; i < dim; i++)
+    {
         if (base[i] == cerca)
-            arrayNuovo[trovaPosizione] = base[i];
-        else
-            arrayNuovo[trovaPosizione] = base[i] + 1;
-    for (int i = 0; i < trovaPosizione; i++)
-        base[i] = arrayNuovo[i];
-    return trovaPosizione;
-
+        {
+            return i + 1;
+        }
+    }
+    return -1;
 
 }
 
-int *aggiornaInPosizione(int base[], int lunghezza, int valore, int posizione) {
-
-
+int *aggiornaInPosizione(int base[], int dim, int valore, int posizione) 
+{
+    if (posizione > dim)
+    {
+        cout << "La posizione si trova al di fuori dell'array";
+        return base;
+    }
+    else
+    {
+        int arrayRis[dim];
+        for (int i = 0; i < posizione - 1; i++)
+        {
+            arrayRis[i] = base[i];
+        }
+        arrayRis[posizione - 1] = valore;
+        for (int i = posizione; i < dim; i++)
+        {
+            arrayRis[i] = base[i];
+        }
+        mostra(arrayRis, dim);
+        return arrayRis;
+    }
 }
+
 
 int *rimuovi(int base[], int lunghezza, int indice)
 {
@@ -110,16 +114,21 @@ int *rimuovi(int base[], int lunghezza, int indice)
 
 int main()
 {
-    int arrayBase[5] = {4, 5, 3, 2, 6};
-    int lunghezzaBase = sizeof(arrayBase) / sizeof(arrayBase[0]);
-    int numero = 8;
-    int posizione=3;
-    int trovaposizione = 3;
-    int *arrayNuovo = aggiungiInFondo(arrayBase, lunghezzaBase, numero);
-    int *arrayNuovo2 = aggiungiInTesta(arrayBase, lunghezzaBase, numero);
-    int* arrayNuovo3 = aggiungiInPosizione (arrayBase,lunghezzaBase, numero, posizione );
-
-
-
+    
+    int base[] = {6, 8, 7, 8, 8, 9};
+    int dim = sizeof(base) / sizeof(base[0]);
+    int nuovo = 11;
+    int posizione = 3;
+    int daTrovare = 8;
+    int *array1 = aggiungiInFondo(base, dim, nuovo);
+    cout << endl;
+     int *array2 = aggiungiInTesta(base, dim, nuovo);
+     cout << endl;
+     int *array3 = aggiungiInPosizione(int base[], int lunghezza, int nuovo, int posizione);
+     cout << endl;
+     cout << "Elemento " << daTrovare << " si trova in posizione " << trovaPosizione(base, dim, daTrovare) << endl;
+     cout << endl;
+    int *array4 = aggiornaInPosizione(base, dim, nuovo, posizione);
+     cout << endl;
+    return 0;
 }
-
